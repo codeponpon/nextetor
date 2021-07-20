47630282d58671bec72a874d759bc9fee3ca8b8d
cPP <cpp@m1.local>
2021-07-20 12:30:34 +0700

    feat: added basePath for game client to access

--- src/components/Layout/Logo/index.tsx
+++ src/components/Layout/Logo/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface Props {
   width: number;
   height: number;
 }
 
-const Logo: React.FC<Props> = ({ width, height }) => {
-  if (!width || !height) {
-    height = width = 200;
-  }
+const Logo: React.FC<Props> = ({ width = 200, height = 200 }) => {
+  const router = useRouter();
 
   return (
-    <Image width={width} height={height} src="/images/logo.png" alt="Logo" />
+    <Image
+      width={width}
+      height={height}
+      src={`${router.basePath}/images/logo.png`}
+      alt="Logo"
+    />
   );
 };
 
